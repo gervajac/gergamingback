54a92e2668cb86bc192cf5b38b8286ff8d3dd5f7
gervajac <gerva.jacob.tropini@gmail.com>
2023-04-04 18:49:31 -0300

    fixerror

--- src/models/items.js
+++ src/models/items.js
@@ -5,10 +5,6 @@ const itemSchema = mongoose.Schema({
         type: String, 
         required: true,
     },
-    description: {
-        type: String,
-        required: true,
-    },
     image: {
         type: String,
         required: true
